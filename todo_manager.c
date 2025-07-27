8fa924b352b70a60ce14b2392d16aa72e6925924
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-07-27 19:07:17 +0200

    i declared some of the function(features) i would want

--- todo_manager.c
+++ todo_manager.c
@@ -17,7 +17,22 @@ typedef struct {
 
 /* ================ FUNCTION DECLARATIONS ================== */
 
+ToDoList* initialize_todo_list(int initial_capacity);
+void add_task(ToDoList *list, const char *title, int priority);
+void remove_task(ToDoList *list, int id);
+void toggle_task_status(ToDoList *list, int id);
+void display_tasks(const ToDoList *list);
+void save_tasks_to_file(const ToDoList *list, const char *filename);
+void load_tasks_from_file(ToDoList *list, const char *filename);
+
+/* =============== FUNCTION IMPLEMENTATIONS ================== */
+
+
 int main(void) {
+    ToDoList *todoList = NULL;
+    int choice = 0;
+    int initial_capacity = 5;
+
 
     return 0;
 }
